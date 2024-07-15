b52d510e156ed7aac5108718c3d4edb08dcc34de
Guilherme Ribeiro <gui921@gmail.com>
2024-07-15 18:53:41 +0100

    Basic App Functionality

---
+++ main.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.geometry("960x640")
+    root.title("DragNN")
+
+    layers_frame = tk.Frame(root, width=200, height=600, bg="#303030")
+    layers_frame.pack(side="left", fill="y")
+
+    build_frame = tk.Frame(root, width=400, height=600, bg="#191919")
+    build_frame.pack(side="right", fill="both", expand=True)
+
+    layers = [
+            "Dense Layer",
+            "Conv2D Layer",
+            "Conv3D Layer",
+            "Transposed Conv2D Layer",
+            "Transposed Conv3D Layer",
+            "Activation Layer",
+            "MaxPool2D Layer",
+            "AvgPool2D Layer",
+            "MaxPool3D Layer",
+            "AvgPool3D Layer",
+            "Flatten Layer",
+            "Dropout Layer",
+            "Dropout2D Layer",
+            "Dropout3D Layer",
+            "BatchNorm2D Layer",
+            "BatchNorm3D Layer"
+        ]
+    
+    for layer in layers:
+        button = tk.Button(layers_frame, text=layer,height=3 ,foreground='#dbdbdb',bg='#3f3f3f')
+        button.pack(fill="x", pady=5)
+        
+    root.mainloop()
+
+
+
+if __name__ == "__main__":
+    main()
